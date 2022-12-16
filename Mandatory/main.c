431c434349c6c86f25fb94f7b080da7a8df1e22e
Ali Boudjemai <alboudje@z3r7p3.42lyon.fr>
2022-12-16 17:19:15 +0100

    Added ILX_Rect, draw_rect and draw_px

--- Mandatory/main.c
+++ Mandatory/main.c
@@ -6,20 +6,54 @@
 /*   By: alboudje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:55:09 by alboudje          #+#    #+#             */
-/*   Updated: 2022/12/16 15:47:10 by alboudje         ###   ########.fr       */
+/*   Updated: 2022/12/16 17:18:11 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 #include <mlx.h>
 
+void	ILX_draw_px(t_ILX_Renderer *rend, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = rend->addr + (y * rend->line_len + x * (rend->bits_per_px >> 3));
+	*(unsigned int*)dst = color;
+}
+
+void	ILX_draw_rect(t_ILX_Renderer *rend, t_ILX_Rect rect, int color)
+{
+	int	w;
+	int	h;
+
+	h = 0;
+	while (h < rect.h)
+	{
+		w = 0;
+		while (w < rect.w)
+		{
+			ILX_draw_px(rend, rect.x + w, rect.y + h, color);
+			w++;
+		}
+		h++;
+	}
+}
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
+	void			*mlx;
+	void			*mlx_win;
+	t_ILX_Renderer	renderer;
+	t_ILX_Rect		rect = {20, 20, 100, 60};
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 800, 600, "MLX");
+	renderer.img = mlx_new_image(mlx, 800, 600);
+	renderer.addr = mlx_get_data_addr(renderer.img, &renderer.bits_per_px, 
+							&renderer.line_len, &renderer.endian);
+	ILX_draw_px(&renderer, 10, 10, 0xff0000);
+	ILX_draw_rect(&renderer, rect, 0xff4444);
+	mlx_put_image_to_window(mlx, mlx_win, renderer.img, 0, 0);
 	mlx_loop(mlx);
 	return (EXIT_SUCCESS);
 }
--- Mandatory/so_long.h
+++ Mandatory/so_long.h
@@ -6,12 +6,32 @@
 /*   By: alboudje <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:53:59 by alboudje          #+#    #+#             */
-/*   Updated: 2022/12/16 13:54:36 by alboudje         ###   ########.fr       */
+/*   Updated: 2022/12/16 17:17:55 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 # include "../libft/libft.h"
+
+typedef struct	s_ILX_Renderer {
+	void	*img;
+	char	*addr;
+	int		bits_per_px;
+	int		line_len;
+	int		endian;
+}	t_ILX_Renderer;
+
+typedef struct	s_ILX_Rect
+{
+	int	x;
+	int	y;
+	int	w;
+	int	h;
+}	t_ILX_Rect;
+
+
+void	ILX_draw_px(t_ILX_Renderer *rend, int x, int y, int color);
+void	ILX_draw_rect(t_ILX_Renderer *rend, t_ILX_Rect rect, int color);
 
 #endif
