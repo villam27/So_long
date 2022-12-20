f599a05330ea1569b4f56a84229b74847cb58eb8
villam27 <villam2703@protonmail.com>
2022-12-21 00:05:27 +0100

    Inproved init_all function

--- Mandatory/main.c
+++ Mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: alboudje <alboudje@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:55:09 by alboudje          #+#    #+#             */
-/*   Updated: 2022/12/20 23:42:01 by alboudje         ###   ########.fr       */
+/*   Updated: 2022/12/21 00:04:33 by alboudje         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,20 @@ static int	next_frame(t_game_data *data)
 	return (0);
 }
 
-static int	init_all(t_ilx_window **win, t_ilx_renderer **ren,
-					t_game_data **game, t_game_input *in)
+static int	init_all(t_game_data **game, t_game_input *in)
 {
-	*win = ilx_create_window(800, 600, "so_long");
-	if (!(*win))
+	t_ilx_window	*win;
+	t_ilx_renderer	*ren;
+	win = ilx_create_window(800, 600, "so_long");
+	if (!(win))
 		return (1);
-	*ren = ilx_create_renderer(*win);
-	if (!(*ren))
+	ren = ilx_create_renderer(win);
+	if (!(ren))
 	{
-		ilx_destroy_window(*win);
+		ilx_destroy_window(win);
 		return (1);
 	}
-	*game = ini_game(*win, *ren, in);
+	*game = ini_game(win, ren, in);
 	if (!(*game))
 		return (1);
 	return (0);
@@ -44,18 +45,16 @@ static int	init_all(t_ilx_window **win, t_ilx_renderer **ren,
 
 int	main(int argc, char **argv)
 {
-	t_ilx_window	*window;
-	t_ilx_renderer	*renderer;
 	t_game_input	inputs = {0, 0, 0, 0, 0};
 	t_game_data		*game;
 
-	if (!init_all(&window, &renderer, &game, &inputs))
+	if (!init_all(&game, &inputs))
 	{
-		mlx_hook(window->mlx_win, 17, 0, win_close, game);
-		mlx_hook(window->mlx_win, 2, 1L << 0, input_key_down, &inputs);
-		mlx_hook(window->mlx_win, 3, 1L << 1, input_key_up, &inputs);
-		mlx_loop_hook(window->mlx, next_frame, game);
-		mlx_loop(window->mlx);
+		mlx_hook(game->win->mlx_win, 17, 0, win_close, game);
+		mlx_hook(game->win->mlx_win, 2, 1L << 0, input_key_down, &inputs);
+		mlx_hook(game->win->mlx_win, 3, 1L << 1, input_key_up, &inputs);
+		mlx_loop_hook(game->win->mlx, next_frame, game);
+		mlx_loop(game->win->mlx);
 	}
 	else
 		return (EXIT_FAILURE);
